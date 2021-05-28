3048cb84b1bbfdff83653de2dd13ba8bb91cec3c
Zhi-Wei <ZhiWei.Lu@outlook.com>
2021-05-28 21:09:50 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace K8sTrainingWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
